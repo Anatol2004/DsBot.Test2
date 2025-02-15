59dc959b7bf167770159070d81191c558490d62d
DEO24 <146115531+DEO2412@users.noreply.github.com>
2025-02-15 20:56:11 +0300

    DsBot.Test2(ver 0.1)

---
+++ DsBot.Test2/DsBot.Test2/Program.cs
@@ -0,0 +1,40 @@
+﻿using DsBot.Test2.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+
+namespace DsBot.Test2
+{
+    internal class Program
+    {
+
+        public static DiscordClient Client { get; set; }
+        public static CommandsNextExtension Commands { get; set; }
+
+        public static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All,
+                AutoReconnect = true
+                 
+            };
+
+            Client = new DiscordClient(discordConfig);
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DsBot.Test2/DsBot.Test2/config/JSONReader.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace DsBot.Test2.config
+{
+    public class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJson()
+        {
+            using (StreamReader sr = new StreamReader("Config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure jSONStructure = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                token = jSONStructure.token;
+                prefix = jSONStructure.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
