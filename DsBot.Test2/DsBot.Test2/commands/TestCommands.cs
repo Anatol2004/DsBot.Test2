81b598d58d747cd162de6c777ff1f899e264262a
DEO24 <146115531+DEO2412@users.noreply.github.com>
2025-02-16 00:16:51 +0300

    DsBot.Test2(ver 0.3)

--- DsBot.Test2/DsBot.Test2/commands/TestCommands.cs
+++ DsBot.Test2/DsBot.Test2/commands/TestCommands.cs
@@ -1,5 +1,7 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using DsBot.Test2.diceGame;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using System.Threading.Tasks;
 
 namespace DsBot.Test2.commands
@@ -12,11 +14,83 @@ namespace DsBot.Test2.commands
             await ctx.Channel.SendMessageAsync("Привет!");
         }
 
+
         [Command("random")]
         public async Task Random(CommandContext ctx, int min, int max)
         {
             var randomValue = new System.Random().Next(min, max);
             await ctx.Channel.SendMessageAsync(ctx.User.Username + " " + randomValue);
+        }
+
+
+        [Command("embed")]
+        public async Task Embed(CommandContext ctx)
+        {
+            var message = new DiscordEmbedBuilder()
+            {
+                Title = "Первый эмбед",
+                Description = $"Данный эмбед создан по запросу {ctx.User.Username}",
+                Color = DiscordColor.Orange
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: message);
+        }
+
+        [Command("dice_game")]
+        public async Task DiceGame(CommandContext ctx)
+        {
+            var playerThrow = new DiceGame();
+            var playerScore = playerThrow.RollSum;
+
+            var message = new DiscordEmbedBuilder()
+            {
+                Title = $"Игрок выбросил: {playerScore}",
+                Color = DiscordColor.Aquamarine
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: message);
+
+            var botThrow = new DiceGame();
+            var botScore = botThrow.RollSum;
+
+            var botMessage = new DiscordEmbedBuilder()
+            {
+                Title = $"Бот выбросил {botScore}",
+                Color = DiscordColor.Aquamarine
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: botMessage);
+
+            if (playerScore > botScore)
+            {
+                var winMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Вы победили!",
+                    Color = DiscordColor.Green
+                };
+
+                await ctx.Channel.SendMessageAsync(embed: winMessage);
+            }
+            else if (playerScore < botScore)
+            {
+                var loseMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Вы проиграли!",
+                    Color = DiscordColor.Red
+                };
+
+                await ctx.Channel.SendMessageAsync(embed: loseMessage);
+            }
+            else
+            {
+                var drawnMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Ничья!",
+                    Color = DiscordColor.Yellow
+                };
+
+                await ctx.Channel.SendMessageAsync(embed: drawnMessage);
+            }
         }
     }
 }
---
+++ DsBot.Test2/DsBot.Test2/diceGame/DiceGame.cs
@@ -0,0 +1,15 @@
+﻿namespace DsBot.Test2.diceGame
+{
+    public class DiceGame
+    {
+        public int RollSum { get; set; }
+
+        public DiceGame()
+        {
+            var random = new System.Random();
+            var firstRoll = random.Next(1, 7);
+            var secondRoll = random.Next(1, 7);
+            RollSum = firstRoll + secondRoll;
+        }
+    }
+}
